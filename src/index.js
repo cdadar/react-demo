4a25d55fa49bc6cd9749c5dcc515ae262ef46d09
jiarenlu <gunbanmi@gmail.com>
2017-05-12 17:15:24 +0800

    add event

--- src/index.js
+++ src/index.js
@@ -5,24 +5,62 @@ import './index.css';
 
 
 class Title extends Component {
- render(){
+   handleClickOnTitle(e){
+    console.log('click on title.')
+    console.log(e.target.innerHTML)
+    console.log(this)
+  }
+  render() {
     return (
-      <h1>React 小书</h1>
+      <h1 onClick={this.handleClickOnTitle(this)}>React 小书</h1>
     )
- }
+  }
 }
 
 class Header extends Component {
   render() {
-    const className = 'header'
-    const isGoodWord = false
     return (
-     <Title />
+      <div>
+        <Title></Title>
+        <h2>This is Header</h2>
+      </div>
+    )
+  }
+}
+
+class Main extends Component{
+  render () {
+    return (
+      <div>
+         <h2>This is main content</h2>
+       </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+         <h2>This is footer</h2>
+       </div>
+    )
+  }
+}
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Header />,
+  <Index />,
   document.getElementById('root')
 )
