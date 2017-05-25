e35444bde6ee22675b10d12715b559fde0d086a2
jiarenlu <gunbanmi@gmail.com>
2017-05-25 11:05:48 +0800

    add react-redux redux

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
-import { Provider } from './react-redux'
-
-function createStore(reducer) {
-    let state = null
-    const listeners = []
-    const subscribe = (listener) => listeners.push(listener)
-    const getState = () => state
-    const dispatch = (action) => {
-        state = reducer(state, action)
-        listeners.forEach((listener) => listener())
-    }
-    dispatch({}) // 初始化 state
-    return { getState, dispatch, subscribe }
-}
 
 const themeReducer = (state, action) => {
-    if (!state) return {
-        themeColor: 'red'
-    }
-    switch (action.type) {
-        case 'CHANGE_COLOR':
-            return { ...state, themeColor: action.themeColor }
-        default:
-            return state
-    }
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
 }
 
 const store = createStore(themeReducer)
