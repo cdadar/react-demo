867d1c53c2ada9a8e048889af32f339e715a6231
jiarenlu <gunbanmi@gmail.com>
2017-05-25 10:51:19 +0800

    add mapStorToProp and mapDispathToProp

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React, { Component, PropTypes } from 'react'
+import { connect } from './react-redux'
 
 class Header extends Component {
     static contextTypes = {
-        store: PropTypes.object
+        themeColor:PropTypes.string
     }
 
     constructor() {
@@ -10,20 +11,17 @@ class Header extends Component {
         this.state = { themeColor: '' }
     }
 
-    componentWillMount() {
-        this._updateThemeColor()
-    }
-
-    _updateThemeColor() {
-        const { store } = this.context
-        const state = store.getState()
-        this.setState({ themeColor: state.themeColor })
-    }
-
     render() {
         return (
-            <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
+            <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+
+Header = connect(mapStateToProps)(Header)
 export default Header
